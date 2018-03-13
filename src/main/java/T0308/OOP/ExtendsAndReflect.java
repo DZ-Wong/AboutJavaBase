c2b55b7e8a88e2bb1dc30965c2f2990f05f0b802
DZ-Wong <dzwang2016@163.com>
2018-03-13 17:56:53 +0800

    Java学习笔记chapter 4.1

---
+++ src/main/java/T0308/OOP/ExtendsAndReflect.java
@@ -0,0 +1,17 @@
+package T0308.OOP;
+
+/**
+ * 继承和反射。
+ * Created by vip on 2018/3/13.
+ */
+public abstract class ExtendsAndReflect {
+    public int i;
+    public static int y;
+    /*实例变量和静态变量统称为域
+    * 类中的域、方法和嵌套类、接口统称为类成员*/
+
+    /*不同于接口，抽象类可以拥有实例变量和构造函数*/
+    public abstract int person();
+
+    /*受保护访问*/
+}
