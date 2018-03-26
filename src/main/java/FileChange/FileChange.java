91b75c6ac2739e038019120e89bde4861ab2210c
DZ-Wong <dzwang2016@163.com>
2018-03-26 17:46:16 +0800

    Java学习 - jaIO

---
+++ src/main/java/FileChange/FileChange.java
@@ -0,0 +1,22 @@
+package FileChange;
+
+import java.io.FileReader;
+
+/**
+ * Created by vip on 2018/3/23.
+ */
+public class FileChange {
+
+    private static FileReader fileReader = null;
+
+//    public static void fileEdit(String path) {
+//        File f = new File(path);
+//        InputStream inputStream = new InputStream() {
+//            @Override
+//            public int read() throws IOException {
+//                return 0;
+//            }
+//        };
+//        InputStreamReader inputStreamReader = new InputStreamReader()
+//    }
+}
---
+++ src/main/java/T0308/JavaIO/IODemo.java
@@ -0,0 +1,8 @@
+package T0308.JavaIO;
+
+/**
+ * Created by vip on 2018/3/26.
+ */
+public class IODemo {
+
+}
