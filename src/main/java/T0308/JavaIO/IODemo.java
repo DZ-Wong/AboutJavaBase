ac8522cf7065cfc9d09ed7ad3f44446342939d2c
DZ-Wong <dzwang2016@163.com>
2018-03-26 18:55:56 +0800

    Java学习 - jaIO

--- src/main/java/T0308/JavaIO/IODemo.java
+++ src/main/java/T0308/JavaIO/IODemo.java
@@ -10,17 +10,42 @@ import java.util.regex.Pattern;
  */
 public class IODemo {
         public static void main(String[] args) {
-            //目录列表器
+            //目录列表器1
             File path = new File(".");
             String[] list;
             if (args.length == 0) {
                 list = path.list();
             } else {
+                //1 创建新类
                 list = path.list(new DirFilter(args[0]));
+                //2 匿名内部类作为单独静态函数
+                list = path.list(filter(args[0]));
+                //3 作为list（）函数的匿名内部类
+                list = path.list(new FilenameFilter() {
+                    private Pattern pattern = Pattern.compile(args[0]);
+
+                    @Override
+                    public boolean accept(File dir, String name) {
+                        return pattern.matcher(name).matches();
+                    }
+                });
             }
             Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
             for (String dirItem : list)
                 System.out.println(dirItem);
+        }
+
+        //2匿名内部类
+    //传向filter（）参数必须是final，在匿名内部类必须的，才能使用来自该类范围之外的对象
+
+        public static FilenameFilter filter(final String regex) {
+            return new FilenameFilter() {
+                private Pattern pattern = Pattern.compile(regex);
+                @Override
+                public boolean accept(File dir, String name) {
+                    return pattern.matcher(name).matches();
+                }
+            };
         }
 }
 
