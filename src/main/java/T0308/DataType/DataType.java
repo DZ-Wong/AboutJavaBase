1d67c64fee173514fc89631c7a44dbed64360e1a
DZ-Wong <dzwang2016@163.com>
2018-03-29 17:58:58 +0800

    Java学习 - 补充

--- src/main/java/T0308/DataType/DataType.java
+++ src/main/java/T0308/DataType/DataType.java
@@ -199,6 +199,34 @@ public class DataType {
 
 
     }
+
+    public static void testInt() {
+        /*1，无论如何，Integer与new Integer不会相等。不会经历拆箱过程，new出来的对象存放在堆，而非new的Integer常量则在常量池（在方法区），他们的内存地址不一样，所以为false。
+
+2，两个都是非new出来的Integer，如果数在-128到127之间，则是true,否则为false。因为java在编译Integer i2 = 128的时候,被翻译成：Integer i2 = Integer.valueOf(128);而valueOf()函数会对-128到127之间的数进行缓存。
+
+3，两个都是new出来的,都为false。还是内存地址不一样。
+
+4，int和Integer(无论new否)比，都为true，因为会把Integer自动拆箱为int再去比。
+*/
+
+        int i = 128;
+        Integer i2 = 128;
+        Integer i3 = new Integer(128);
+        System.out.println(i == i2); //Integer会自动拆箱为int，所以为true
+        System.out.println(i == i3); //true，理由同上
+        Integer i4 = 127;//编译时被翻译成：Integer i4 = Integer.valueOf(127);
+        Integer i5 = 127;
+        System.out.println(i4 == i5);//true
+        Integer i6 = 128;
+        Integer i7 = 128;
+        System.out.println(i6 == i7);//false
+        Integer i8 = new Integer(127);
+        System.out.println(i5 == i8); //false
+        Integer i9 = new Integer(128);
+        Integer i10 = new Integer(123);
+        System.out.println(i9 == i10);  //false
+    }
     /**
      * Object 类型:equals, hashcode
      */
--- src/main/java/T0308/OOP/ProxyDemo.java
+++ src/main/java/T0308/OOP/ProxyDemo.java
@@ -15,6 +15,32 @@ public class ProxyDemo {
 
     /*将额外操作从实际对象中分离到不同的地方，能快速插入和删除这些操作*/
 
+    /**
+     *
+     AOP，是通过动态代理实现的。
+
+     一、简单来说：
+
+     　　JDK动态代理只能对实现了接口的类生成代理，而不能针对类
+
+     　　CGLIB是针对类实现代理，主要是对指定的类生成一个子类，覆盖其中的方法（继承）
+
+     二、Spring在选择用JDK还是CGLiB的依据：
+
+     (1)当Bean实现接口时，Spring就会用JDK的动态代理
+
+     (2)当Bean没有实现接口时，Spring使用CGlib是实现
+
+     　  (3)可以强制使用CGlib（在spring配置中加入<aop:aspectj-autoproxy proxy-target-class="true"/>）
+
+     三、CGlib比JDK快？
+
+     　 (1)使用CGLib实现动态代理，CGLib底层采用ASM字节码生成框架，使用字节码技术生成代理类，比使用Java反射效率要高。唯一需要注意的是，CGLib不能对声明为final的方法进行代理，因为CGLib原理是动态生成被代理类的子类。
+
+     　 (2)在对JDK动态代理与CGlib动态代理的代码实验中看，1W次执行下，JDK7及8的动态代理性能比CGlib要好20%左右。
+     * @param iface
+     */
+
     //静态代理
     //执行代理对象的方法
     public static void consumer(Interface iface) {
